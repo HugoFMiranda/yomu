59c4b1294f9edd1860ca51de3f0293aa4071c6ef
HugoFMiranda <hugoeu10@gmail.com>
2026-07-18 14:24:59 +0000

    chore(release): bump version to 1.7.14

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,8 +4,8 @@ object AndroidVersions {
     const val compileSdk = 35
     const val minSdk = 23
     const val targetSdk = 34
-    const val versionCode = 119
-    const val versionName = "1.7.13"
+    const val versionCode = 120
+    const val versionName = "1.7.14"
     const val ndk = "23.1.7779620"
     const val kotlin = "2.1.21"
 }
